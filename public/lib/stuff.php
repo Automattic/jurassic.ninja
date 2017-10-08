2d40a71d4439026e4c45f3530990bbddce0f945c
oskosk <oskosk@gmail.com>
2017-10-08 11:05:34 -0300

    Replace usage of literal array of chars for merge w/range()

--- public/lib/stuff.php
+++ public/lib/stuff.php
@@ -40,11 +40,7 @@ function random_string( $length = 32 ) {
 	$randstr;
 	srand( (double) microtime( true ) * 1000000 );
 	//our array add all letters and numbers if you wish
-	$chars = array(
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p',
-		'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5',
-		'6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+	$chars = array_merge( range( 'a', 'z' ), range( 0, 9 ), range( 'A', 'Z' ) );
 
 	for ( $rand = 0; $rand <= $length; $rand++ ) {
 		$random = rand( 0, count( $chars ) - 1 );
