628c6211d6c8d54db2aec05add67d0179f56036f
oskosk <oskosk@gmail.com>
2017-10-10 00:17:51 -0300

    Add plugin

---
+++ jurassic.ninja.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Plugin Name: Jurassic Ninja
+ * Plugin URI:  https://jurassic.ninja
+ * Description: Launch All The Freedom
+ * Version: 1.0
+ * Author: Osk
+ * Author URI: https://oskosk.wordpress.com
+ * */
+
+require_once __DIR__ . '/lib/stuff.php';
