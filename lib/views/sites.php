24014a5341d35d6c47febf89fbfecc849488ed60
oskosk <oskosk@gmail.com>
2017-10-10 22:06:16 -0300

    Better data in the sites table

--- lib/views/sites.php
+++ lib/views/sites.php
@@ -4,11 +4,13 @@ namespace jn;
 
 require_once __DIR__ . '/../db-stuff.php';
 
-$sites = db()->get_results( 'select * from sites', \ARRAY_A );
+$db_sites = db()->get_results( 'select * from sites', \ARRAY_A );
 
-$just_site_domains = array_column( $sites, 'domain' );
+$just_site_domains = array_column( $db_sites, 'domain' );
 
-$sites_from_serverpilot = array_filter( sp()->app_list()->data, function ( $site ) {
+$db_sites_indexed = array_combine( $just_site_domains, $db_sites );
+
+$server_pilot_apps = array_filter( sp()->app_list()->data, function ( $site ) {
 	return 'jurassic.ninja' !== $site->name;
 } );
 
@@ -16,22 +18,29 @@ $sites_from_serverpilot = array_filter( sp()->app_list()->data, function ( $site
 <table class="fixed widefat">
 	<thead>
 		<tr>
-			<th class="manage-column column-columnname"><?php echo esc_html( 'Site' ); ?> </th>
-			<th class="manage-column column-columnname"><?php echo esc_html( 'System user' ); ?> </th>
-			<th class="manage-column column-columnname"><?php echo esc_html( 'Exists in logs' ); ?> </th>
+			<th class="manage-column column-columnname"><?php echo esc_html_e( '#' ); ?> </th>
+			<th class="manage-column column-columnname"><?php echo esc_html_e( 'Site' ); ?> </th>
+			<th class="manage-column column-columnname"><?php echo esc_html_e( 'System user' ); ?> </th>
+			<th class="manage-column column-columnname"><?php echo esc_html_e( 'Exists in logs' ); ?> </th>
+			<th class="manage-column column-columnname"><?php echo esc_html_e( 'Created on' ); ?> </th>
 		</tr>
 	</thead>
 	<tbody>
 <?php
-foreach ( $sites_from_serverpilot as $site ) {
-	$in_logs = in_array( $site->domains[0], $just_site_domains, true );
+foreach ( $server_pilot_apps as $site ) {
+	$domain = $site->domains[0];
+	$in_logs = array_key_exists( $domain, $db_sites_indexed );
+	$db_id = $in_logs ? $db_sites_indexed[ $domain ]['id'] : '';
+	$created = $in_logs ? $db_sites_indexed[ $domain ]['created'] : '';
 	?>
 	<tr class="active">
+		<td class="column-columnname"><?php echo esc_html( $db_id ); ?></td>
 		<td class="column-columnname">
-			<a target="_blank" href="<?php echo 'http://' . esc_attr( $site->domains[0] ); ?>" rel="noopener nofollow"<strong><?php echo esc_html( $site->domains[0] ); ?></strong></a>
+			<a target="_blank" href="<?php echo 'http://' . esc_attr( $domain ); ?>" rel="noopener nofollow"<strong><?php echo esc_html( $domain ); ?></strong></a>
 		</td>
 		<td class="column-columnname"><?php echo esc_html( $site->name ); ?></td>
-		<td class="column-columnname"><?php echo esc_html( $in_logs ? __( 'Yes' ) : _( 'No' ) ) ; ?></td>
+		<td class="column-columnname"><?php echo $in_logs ? esc_html_e( 'Yes' ) : esc_html_e( 'No' ); ?></td>
+		<td class="column-columnname"><?php echo esc_html( $created ); ?></td>
 	</tr>
 	<?php
 }
