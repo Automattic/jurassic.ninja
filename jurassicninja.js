0e30e591505b047c5afc756debca18916bdf2f65
oskosk <oskosk@gmail.com>
2018-02-12 23:54:18 -0300

    create jurassicNinjaApi class

--- jurassicninja.js
+++ jurassicninja.js
@@ -9,7 +9,7 @@ function doIt( $, features ) {
 		$( '#img1' ).show();
 		$( '#img2' ).hide();
 
-		createSite( features )
+		jurassicNinjaApi().launchSite( features )
 			.then( response => {
 				$( '#progress' ).html( `<a href="${ response.data.url }">The new WP is ready to go, visit it!</a>` );
 				$( '#img1' ).hide();
@@ -28,7 +28,7 @@ function doItWithFeatures( $, features ) {
 		$( '#progress' ).show();
 		$( '#img1' ).show();
 		$( '#img2').hide();
-		launchSiteWithFeatures( features )
+		jurassicNinjaApi().launchSiteWithFeatures( features )
 			.then( response => {
 				$( '#progress' ).html( `<a href="${ response.data.url }">The new WP is ready to go, visit it!</a>` );
 				$( '#img1' ).hide();
@@ -42,57 +42,69 @@ function doItWithFeatures( $, features ) {
 	} );
 }
 
-function createSite( features ) {
-	const url = restApiSettings.root;
-	const nonce = restApiSettings.nonce;
-	return fetch( url + 'jurassic.ninja/create', {
-		method: 'post',
-		credentials: 'same-origin',
-		body: Object.keys( features ).length ? JSON.stringify( features ) : null,
-		headers: {
-			'X-WP-Nonce': nonce,
-			'content-type': 'application/json',
-		}
-	} )
+function jurassicNinjaApi() {
+	if ( ! ( this instanceof jurassicNinjaApi ) ) {
+		return new jurassicNinjaApi();
+	}
+
+	function launchSite( features ) {
+		const url = restApiSettings.root;
+		const nonce = restApiSettings.nonce;
+		return fetch( url + 'jurassic.ninja/create', {
+			method: 'post',
+			credentials: 'same-origin',
+			body: Object.keys( features ).length ? JSON.stringify( features ) : null,
+			headers: {
+				'X-WP-Nonce': nonce,
+				'content-type': 'application/json',
+			}
+		} )
 		.then( checkStatusAndErrors ).then( parseJson )
-}
+	}
 
-function launchSiteWithFeatures( features ) {
-	const url = restApiSettings.root;
-	const nonce = restApiSettings.nonce;
-	return fetch( url + 'jurassic.ninja/specialops/create', {
-		method: 'post',
-		credentials: 'same-origin',
-		body: JSON.stringify( features ),
-		headers: {
-			'X-WP-Nonce': nonce,
-			'content-type': 'application/json',
-		}
-	} )
+	function launchSiteWithFeatures( features ) {
+		const url = restApiSettings.root;
+		const nonce = restApiSettings.nonce;
+		return fetch( url + 'jurassic.ninja/specialops/create', {
+			method: 'post',
+			credentials: 'same-origin',
+			body: JSON.stringify( features ),
+			headers: {
+				'X-WP-Nonce': nonce,
+				'content-type': 'application/json',
+			}
+		} )
 		.then( checkStatusAndErrors ).then( parseJson )
 
-	;
-}
+		;
+	}
 
-function checkStatusAndErrors( response ) {
-	if ( response.status === 503 ) {
-		throw new Error( 'Site launching is turned off right now' );
+	function checkStatusAndErrors( response ) {
+		if ( response.status === 503 ) {
+			throw new Error( 'Site launching is turned off right now' );
+		}
+		if ( response.status === 403 ) {
+			throw new Error( 'Launching sites is currently restricted to authenticated users' );
+		}
+		if ( response.status !== 200 ) {
+			throw new Error( 'The API responded in a weird way' );
+		}
+		return response;
 	}
-	if ( response.status === 403 ) {
-		throw new Error( 'Launching sites is currently restricted to authenticated users' );
-	}
-	if ( response.status !== 200 ) {
-		throw new Error( 'The API responded in a weird way' );
-	}
-	return response;
-}
 
-function parseJson( response ) {
-	return response.json()
+	function parseJson( response ) {
+		return response.json()
 		.catch( () => {
 			throw new Error ( 'There was en error with the response from the API' )
 		} );
+	}
+
+	this.launchSite = launchSite;
+	this.launchSiteWithFeatures = launchSiteWithFeatures;
 }
+
+
+
 
 function collectFeatures() {
 	const reduce = Array.prototype.reduce;
