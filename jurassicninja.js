3a14efb4acfabe1e4ea7b8000fcc21b3908f75a8
Osk <oskosk@users.noreply.github.com>
2018-02-13 01:08:29 -0300

    Add filters and actions to allow for a more modular approach for feature addition (#91)
    * Add handy filters and actions for allowing more modular hooking when
    adding features for launched sites
    
    * remove unnecessary feature defaults from js code
    
    * create jurassicNinjaApi class
    
    * introduce init() js function
    
    * use better naming for some js functions
    
    * move api client down
    
    * rename collectFeatures to collectFeaturesFromFormInputs
    
    * use startSpinner and stopSpinner functions for less duplication of code

--- jurassicninja.js
+++ jurassicninja.js
@@ -2,115 +2,89 @@
 const CREATE_PAGE_SLUG = '/create';
 const SPECIALOPS_CREATE_PAGE_SLUG = '/specialops';
 
-const defaultFeatures = {
-	'runtime': 'php7.0',
-	'ssl': false,
-	'jetpack': false,
-	'jetpack-beta': false,
-	'subdir_multisite': false,
-	'subdomain_multisite': false,
-	'wordpress-beta-tester': false,
-	'config-constants': false,
-	'wp-debug-log': false,
-	'wp-log-viewer': false,
-	'gutenberg': false,
-	'woocommerce': false,
-};
-
 const originalProgressText = jQuery( '#progress' ).text();
 
-function doIt( $, features ) {
-	$( function() {
-		$( '#img1' ).show();
-		$( '#img2' ).hide();
+init();
 
-		createSite( features )
+function init() {
+	if ( isCreatePage() ) {
+		const shortlived = param( 'shortlived' );
+		const jetpack = param( 'jetpack' );
+		const woocommerce = param( 'woocommerce' );
+		const jetpack_beta = param( 'jetpack-beta' );
+		const wp_debug_log = param( 'wp-debug-log' )
+		const branch = param( 'branch' )
+		const features = {};
+		if ( shortlived !== null ) {
+			features.shortlived = shortlived;
+		}
+		if ( jetpack !== null ) {
+			features.jetpack = jetpack;
+		}
+		if ( woocommerce !== null ) {
+			features.woocommerce = woocommerce;
+		}
+		if ( wp_debug_log !== null ) {
+			features[ 'wp-debug-log' ] = wp_debug_log;
+		}
+		if ( jetpack_beta !== null ) {
+			features[ 'jetpack-beta' ] = jetpack_beta;
+			features.branch = ( branch !== null ? branch : 'stable' );
+		}
+
+		setTimeout( () => {
+			launchSite( jQuery, features );
+		}, 1000 );
+	}
+
+	if ( isSpecialOpsPage() ) {
+		jQuery( '[data-is-create-button]' ).click( function () {
+			const $this = jQuery( this );
+			const features = collectFeaturesFromFormInputs();
+			// Buttons can declare a feature too
+			if ( $this.data( 'feature' )  ) {
+				features[ $this.data( 'feature' ) ] = true;
+			}
+			launchSiteWithFeatures( jQuery, features );
+			return false;
+		} )
+	}
+}
+
+function launchSite( $, features ) {
+	$( function() {
+		startSpinner();
+
+		jurassicNinjaApi().create( features )
 			.then( response => {
 				$( '#progress' ).html( `<a href="${ response.data.url }">The new WP is ready to go, visit it!</a>` );
-				$( '#img1' ).hide();
-				$( '#img2' ).show();
+				stopSpinner();
 			} )
 			.catch( err => {
 				$( '#progress' ).text( `Oh No! There was a problem launching the new WP. (${ err.message }).` );
-				$( '#img2' ).hide();
-				$( '#img1' ).attr( 'src', 'https://i.imgur.com/vdyaxmx.gif' );
+				stopSpinner( true );
 			} );
 	} );
 }
 
-function doItWithFeatures( $, features ) {
+function launchSiteWithFeatures( $, features ) {
 	$( function() {
+		$( '#progress' ).text( originalProgressText );
 		$( '#progress' ).show();
-		$( '#img1' ).show();
-		$( '#img2').hide();
-		launchSiteWithFeatures( features )
+		startSpinner();
+		jurassicNinjaApi().specialops( features )
 			.then( response => {
 				$( '#progress' ).html( `<a href="${ response.data.url }">The new WP is ready to go, visit it!</a>` );
-				$( '#img1' ).hide();
-				$( '#img2' ).show();
+				stopSpinner();
 			} )
 			.catch( err => {
 				$( '#progress' ).text( `Oh No! There was a problem launching the new WP. (${ err.message }).` );
-				$( '#img2' ).hide();
-				$( '#img1' ).attr( 'src', 'https://i.imgur.com/vdyaxmx.gif' );
+				stopSpinner( true );
 			} );
 	} );
 }
 
-function createSite( features ) {
-	const url = restApiSettings.root;
-	const nonce = restApiSettings.nonce;
-	return fetch( url + 'jurassic.ninja/create', {
-		method: 'post',
-		credentials: 'same-origin',
-		body: Object.keys( features ).length ? JSON.stringify( features ) : null,
-		headers: {
-			'X-WP-Nonce': nonce,
-			'content-type': 'application/json',
-		}
-	} )
-		.then( checkStatusAndErrors ).then( parseJson )
-}
-
-function launchSiteWithFeatures( features ) {
-	features = Object.assign( {}, defaultFeatures, features );
-	const url = restApiSettings.root;
-	const nonce = restApiSettings.nonce;
-	return fetch( url + 'jurassic.ninja/specialops/create', {
-		method: 'post',
-		credentials: 'same-origin',
-		body: JSON.stringify( features ),
-		headers: {
-			'X-WP-Nonce': nonce,
-			'content-type': 'application/json',
-		}
-	} )
-		.then( checkStatusAndErrors ).then( parseJson )
-
-	;
-}
-
-function checkStatusAndErrors( response ) {
-	if ( response.status === 503 ) {
-		throw new Error( 'Site launching is turned off right now' );
-	}
-	if ( response.status === 403 ) {
-		throw new Error( 'Launching sites is currently restricted to authenticated users' );
-	}
-	if ( response.status !== 200 ) {
-		throw new Error( 'The API responded in a weird way' );
-	}
-	return response;
-}
-
-function parseJson( response ) {
-	return response.json()
-		.catch( () => {
-			throw new Error ( 'There was en error with the response from the API' )
-		} );
-}
-
-function collectFeatures() {
+function collectFeaturesFromFormInputs() {
 	const reduce = Array.prototype.reduce;
 	const els = jQuery( 'input[type=checkbox][data-feature]' );
 	const features = reduce.call( els, function( acc, el ) {
@@ -131,50 +105,18 @@ function isCreatePage() {
 	return window.location.pathname.startsWith( CREATE_PAGE_SLUG )
 }
 
-if ( isCreatePage() ) {
-	const shortlived = param( 'shortlived' );
-	const jetpack = param( 'jetpack' );
-	const woocommerce = param( 'woocommerce' );
-	const jetpack_beta = param( 'jetpack-beta' );
-	const wp_debug_log = param( 'wp-debug-log' )
-	const branch = param( 'branch' )
-	const features = {};
-	if ( shortlived !== null ) {
-		features.shortlived = shortlived;
-	}
-	if ( jetpack !== null ) {
-		features.jetpack = jetpack;
-	}
-	if ( woocommerce !== null ) {
-		features.woocommerce = woocommerce;
-	}
-	if ( wp_debug_log !== null ) {
-		features[ 'wp-debug-log' ] = wp_debug_log;
-	}
-	if ( jetpack_beta !== null ) {
-		features[ 'jetpack-beta' ] = jetpack_beta;
-		features.branch = ( branch !== null ? branch : 'stable' );
-	}
-
-	setTimeout( () => {
-		doIt( jQuery, features );
-	}, 1000 );
+function startSpinner() {
+	jQuery( '#img1' ).show();
+	jQuery( '#img2' ).hide();
 }
-
-if ( isSpecialOpsPage() ) {
-	jQuery( '[data-is-create-button]' ).click( function () {
-		jQuery( '#img1' ).show();
+function stopSpinner( error = false ) {
+	if ( error ) {
 		jQuery( '#img2' ).hide();
-		jQuery( '#progress' ).text( originalProgressText );
-		const $this = jQuery( this );
-		const features = collectFeatures();
-		// Buttons can declare a feature too
-		if ( $this.data( 'feature' )  ) {
-			features[ $this.data( 'feature' ) ] = true;
-		}
-		doItWithFeatures( jQuery, features );
-		return false;
-	} )
+		jQuery( '#img1' ).attr( 'src', 'https://i.imgur.com/vdyaxmx.gif' );
+	} else {
+		jQuery( '#img1' ).hide();
+		jQuery( '#img2' ).show();
+	}
 }
 
 function param( name ) {
@@ -195,4 +137,65 @@ function param( name ) {
 		return null;
 	}
 	return null;
+}
+
+function jurassicNinjaApi() {
+	if ( ! ( this instanceof jurassicNinjaApi ) ) {
+		return new jurassicNinjaApi();
+	}
+
+	function create( features ) {
+		const url = restApiSettings.root;
+		const nonce = restApiSettings.nonce;
+		return fetch( url + 'jurassic.ninja/create', {
+			method: 'post',
+			credentials: 'same-origin',
+			body: Object.keys( features ).length ? JSON.stringify( features ) : null,
+			headers: {
+				'X-WP-Nonce': nonce,
+				'content-type': 'application/json',
+			}
+		} )
+		.then( checkStatusAndErrors ).then( parseJson )
+	}
+
+	function specialops( features ) {
+		const url = restApiSettings.root;
+		const nonce = restApiSettings.nonce;
+		return fetch( url + 'jurassic.ninja/specialops/create', {
+			method: 'post',
+			credentials: 'same-origin',
+			body: JSON.stringify( features ),
+			headers: {
+				'X-WP-Nonce': nonce,
+				'content-type': 'application/json',
+			}
+		} )
+		.then( checkStatusAndErrors ).then( parseJson )
+
+		;
+	}
+
+	function checkStatusAndErrors( response ) {
+		if ( response.status === 503 ) {
+			throw new Error( 'Site launching is turned off right now' );
+		}
+		if ( response.status === 403 ) {
+			throw new Error( 'Launching sites is currently restricted to authenticated users' );
+		}
+		if ( response.status !== 200 ) {
+			throw new Error( 'The API responded in a weird way' );
+		}
+		return response;
+	}
+
+	function parseJson( response ) {
+		return response.json()
+		.catch( () => {
+			throw new Error ( 'There was en error with the response from the API' )
+		} );
+	}
+
+	this.create = create;
+	this.specialops = specialops;
 }
