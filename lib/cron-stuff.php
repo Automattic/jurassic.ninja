f9526c99397324f37224b5a94055d2857eb35a31
oskosk <oskosk@gmail.com>
2018-03-13 17:40:35 -0300

    Run the purge cron every fifteen minutes

--- lib/cron-stuff.php
+++ lib/cron-stuff.php
@@ -15,8 +15,12 @@ require_once __DIR__ . '/settings-stuff.php';
  * @param  string $plugin_file Nothing super useful. the plugin path
  */
 function add_cron_job( $plugin_file ) {
+	// Register a new fifteen-minutes interval
+	add_filter( 'cron_schedules', 'jn\add_cron_recurrence_interval' );
+
+	// Register the cron task if not there already
 	if ( ! wp_next_scheduled( 'jurassic_ninja_purge' ) ) {
-		wp_schedule_event( time(), 'hourly', 'jurassic_ninja_purge' );
+		wp_schedule_event( time(), 'every_fifteen_minutes', 'jurassic_ninja_purge' );
 	}
 
 	add_action( 'jurassic_ninja_purge', 'jn\jurassic_ninja_purge_cron_task' );
@@ -48,4 +52,13 @@ function jurassic_ninja_purge_cron_task() {
  */
 function jurassic_ninja_cron_task_deactivation() {
 	wp_clear_scheduled_hook( 'jurassic_ninja_purge' );
+}
+
+function add_cron_recurrence_interval( $schedules ) {
+	$schedules['every_fifteen_minutes'] = [
+		'interval'  => 15 * \MINUTE_IN_SECONDS,
+		'display'   => __( 'Every 15 Minutes', 'jurassic-ninja' ),
+	];
+
+	return $schedules;
 }
