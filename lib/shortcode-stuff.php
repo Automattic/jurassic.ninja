c9d16c881ba04cc5591c95c91af9bc8829e1d771
oskosk <oskosk@gmail.com>
2018-09-16 11:29:50 -0300

    simplify to only one shortcode

--- lib/shortcode-stuff.php
+++ lib/shortcode-stuff.php
@@ -9,8 +9,9 @@ if ( ! defined( '\\ABSPATH' ) ) {
 function add_shortcodes( $file ) {
 	add_action( 'init', function() use ( $file ) {
 		add_shortcode( 'jurassic_ninja_launch_page', function() use ( $file ) {
-			$ret = include( dirname( $file ) . '/lib/views/launch-site-shortcode.php' );
-			return $ret;
+			ob_start();
+			include( dirname( $file ) . '/lib/views/launch-site-shortcode.php' );
+			return ob_get_clean();
 		} );
 	} );
 }
