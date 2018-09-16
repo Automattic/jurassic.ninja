aeb709d03e0459a57836ebb718580badd2578002
oskosk <oskosk@gmail.com>
2018-09-16 09:55:26 -0300

    bump to 4.0.1

--- jurassic.ninja.php
+++ jurassic.ninja.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name: Jurassic Ninja
  * Description: Launch ephemeral instances of WordPress + Jetpack using ServerPilot and an Ubuntu Box.
- * Version: 4.0
+ * Version: 4.0.1
  * Author: Automattic
  **/
 
