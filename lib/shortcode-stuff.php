b1b49ce51bd2120a78e36dc9edee7188746db17e
oskosk <oskosk@gmail.com>
2018-09-16 11:42:41 -0300

    use atts for shortcode

--- lib/shortcode-stuff.php
+++ lib/shortcode-stuff.php
@@ -8,8 +8,15 @@ if ( ! defined( '\\ABSPATH' ) ) {
 
 function add_shortcodes( $file ) {
 	add_action( 'init', function() use ( $file ) {
-		add_shortcode( 'jurassic_ninja_launch_page', function() use ( $file ) {
+		add_shortcode( 'jurassic_ninja_launch_page', function( $atts ) use ( $file ) {
 			ob_start();
+			$defaults = [
+				'success_message' => __( 'The new WP is ready to go, visit it!', 'jurassic-ninja' ),
+				'failure_message' => __( 'Oh No! There was a problem launching the new WP.', 'jurassic-ninja' ),
+				'failure_image' => __( 'https://i.imgur.com/vdyaxmx.gif', 'jurassic-ninja' ),
+				'spinner_message' => __( 'Launching a fresh WP with a Jetpack ...', 'jurassic-ninja' ),
+			];
+			$atts = array_merge( $defaults, $atts );
 			include( dirname( $file ) . '/lib/views/launch-site-shortcode.php' );
 			return ob_get_clean();
 		} );
--- lib/views/launch-site-shortcode.php
+++ lib/views/launch-site-shortcode.php
@@ -1,3 +1,7 @@
-<img id="img1" class="aligncenter" style="display: none;" data-failure-img-src="https://i.imgur.com/vdyaxmx.gif" src="https://i.imgur.com/mKwWJQZ.gif" />
+<img id="img1" class="aligncenter" style="display: none;" data-failure-img-src="<?php echo esc_attr( $atts['failure_image'] ); ?>" src="https://i.imgur.com/mKwWJQZ.gif" />
 <img id="img2" class="aligncenter" style="display: none;" src="https://i.imgur.com/wWkoZGw.gif" />
-<p id="progress" style="text-align: center;" data-success-message="The new WP is ready to go, visit it!" data-error-message="Oh No! There was a problem launching the new WP.">Launching a fresh WP with a Jetpack ...</p>
+<p id="progress" style="text-align: center;"
+	data-success-message="<?php echo esc_attr( echo $atts['success_message'] ); ?>"
+	data-error-message="<?php echo esc_attr( echo $atts['failure_message'] ); ?>">
+	<?php echo esc_html( $atts['spinner_message'] );?>
+</p>
