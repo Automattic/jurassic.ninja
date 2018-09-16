53f60e6ea6daba07ec60f4579d2fc966b0661fbb
oskosk <oskosk@gmail.com>
2018-09-16 11:45:19 -0300

    use atts for shortcode

--- lib/views/launch-site-shortcode.php
+++ lib/views/launch-site-shortcode.php
@@ -1,7 +1,7 @@
 <img id="img1" class="aligncenter" style="display: none;" data-failure-img-src="<?php echo esc_attr( $atts['failure_image'] ); ?>" src="https://i.imgur.com/mKwWJQZ.gif" />
 <img id="img2" class="aligncenter" style="display: none;" src="https://i.imgur.com/wWkoZGw.gif" />
 <p id="progress" style="text-align: center;"
-	data-success-message="<?php echo esc_attr( echo $atts['success_message'] ); ?>"
-	data-error-message="<?php echo esc_attr( echo $atts['failure_message'] ); ?>">
+	data-success-message="<?php echo esc_attr( $atts['success_message'] ); ?>"
+	data-error-message="<?php echo esc_attr( $atts['failure_message'] ); ?>">
 	<?php echo esc_html( $atts['spinner_message'] );?>
 </p>
