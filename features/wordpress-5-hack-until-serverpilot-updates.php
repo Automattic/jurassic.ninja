f9d1af2bd302b1688fc60fa102271c93ee8d50db
oskosk <oskosk@gmail.com>
2018-12-06 15:28:47 -0300

    Just use wp core update

--- features/wordpress-5-hack-until-serverpilot-updates.php
+++ features/wordpress-5-hack-until-serverpilot-updates.php
@@ -3,9 +3,6 @@
 namespace jn;
 
 add_action( 'jurassic_ninja_init', function() {
-	$defaults = [
-		'wordpress-5' => false,
-	];
 
 	add_action( 'jurassic_ninja_add_features_before_auto_login', function( &$app = null, $features, $domain ) {
 		if (
@@ -28,17 +25,9 @@ add_action( 'jurassic_ninja_admin_init', function() {
 				'id' => 'wordpress_5_hack',
 				'title' => __( 'Force launching of WordPress 5', 'jurassic-ninja' ),
 				'text' => __( 'Until ServerPilot starts installing WordPress 5, we need this hack.', 'jurassic-ninja' ),
-				'placeholder' => '5.0',
 				'type' => 'checkbox',
 				'checked' => false,
-			],
-			'wordpress_5_latest' => [
-				'id' => 'wordpress_5_latest',
-				'title' => __( 'Latest tag for WordPress 5.0', 'jurassic-ninja' ),
-				'text' => __( 'Which version to run when wordpress-5 requested', 'jurassic-ninja' ),
-				'placeholder' => '5.0',
-				'value' => '5.0',
-			],
+			]
 		];
 		return array_merge( $fields, $field );
 	}, 10 );
@@ -48,8 +37,7 @@ add_action( 'jurassic_ninja_admin_init', function() {
  * Updates WordPress to latest stable available for 5.0.
  */
 function update_to_wordpress_5_latest() {
-	$wordpress_5_latest = settings( 'wordpress_5_latest', '5.0' );
-	$cmd = "wp core update --version=$wordpress_5_latest && wp core update-db";
+	$cmd = "wp core update && wp core update-db";
 	add_filter( 'jurassic_ninja_feature_command', function ( $s ) use ( $cmd ) {
 		return "$s && $cmd";
 	} );
