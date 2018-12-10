59975bb08e9b591df389e887d4bf4a77fc15e969
oskosk <oskosk@gmail.com>
2018-12-10 13:50:39 -0300

    Update npm ci command to npx lerna bootstrap --ci

--- bin/build-gutenpack.sh
+++ bin/build-gutenpack.sh
@@ -13,7 +13,7 @@ echo "Getting wp-calypso branch $CALYPSO_BRANCH"
 cd $HOME \
 && git clone https://github.com/automattic/wp-calypso --depth=1 -b "$CALYPSO_BRANCH" \
 && cd wp-calypso \
-&& npm ci --production \
+&& npx lerna bootstrap --ci \
 && echo "Building jetpack-editor for wp-calypso branch $CALYPSO_BRANCH" \
 && npm run sdk -- gutenberg client/gutenberg/extensions/presets/jetpack \
   --output-dir=$HOME/apps/$USER/public/wp-content/plugins/"$JETPACK_DIRNAME"/_inc/blocks \
