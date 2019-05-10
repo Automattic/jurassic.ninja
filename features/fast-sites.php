94859be238bcd46329573433fd2ddb320cff8031
oskosk <oskosk@gmail.com>
2019-05-10 14:33:59 -0300

    .

--- features/fast-sites.php
+++ features/fast-sites.php
@@ -2,31 +2,32 @@
 
 namespace jn;
 
-add_action( 'jurassic_ninja_init', function() {
-	add_action( 'jurassic_ninja_added_rest_api_endpoints', function() {
-		add_post_endpoint( 'apps', function ( $request ) {
-			$body = $request->get_json_params() ? $request->get_json_params() : [];
-			// phpcs:disable WordPress.WP.DeprecatedFunctions.generate_random_passwordFound
-			$username = generate_random_username();
-			$password = generate_random_password();
-			try {
-				$sysuser = create_sp_sysuser( $username, $password );
-			} catch ( \Exception $e ) {
-				$sysuser = new \WP_Error( $e->getCode(), $e->getMessage() );
-				return $sysuser;
-			}
+add_action( 'jurassic_ninja_added_rest_api_endpoints', function() {
+	add_post_endpoint( 'apps', function ( $request ) {
+		$body = $request->get_json_params() ? $request->get_json_params() : [];
+		// phpcs:disable WordPress.WP.DeprecatedFunctions.generate_random_passwordFound
+		$username = generate_random_username();
+		$password = generate_random_password();
+		try {
+			$sysuser = create_sp_sysuser( $username, $password );
+		} catch ( \Exception $e ) {
+			$sysuser = new \WP_Error( $e->getCode(), $e->getMessage() );
+			return $sysuser;
+		}
 
-			$appname = $sysuser->data->name;
-			$php_version = 'php7.3';
-			$domain = sprintf( '%s.%s', $appname, settings( 'domain' ) );
-			$domain_arg = [ $domain ];
-			$app = create_sp_app( $appname, $sysuser->data->id, $php_version, $domain_arg, [] );
+		$appname = $sysuser->data->name;
+		$php_version = 'php7.3';
+		$domain = sprintf( '%s.%s', $appname, settings( 'domain' ) );
+		$domain_arg = [ $domain ];
+		$app = create_sp_app( $appname, $sysuser->data->id, $php_version, $domain_arg, [] );
 
-			$output = [
-				'url' => $app,
-			];
+		$output = [
+			'url' => $app,
+		];
 
-			return $output;
-		} );
+		return $output;
 	} );
+} );
+
+add_action( 'jurassic_ninja_init', function() {
 } );
