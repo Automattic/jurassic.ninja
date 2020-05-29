ba76bf7c85f3d4b974ba836491a47cff5a0b327d
oskosk <oskosk@gmail.com>
2020-05-29 15:51:30 +0000

    Allow to launch a spare site from cli

--- lib/cli-stuff.php
+++ lib/cli-stuff.php
@@ -18,6 +18,8 @@ class JN_CLI_Command extends \WP_CLI_Command {
 			if ( $args[0] === 'spare' ) {
 				\WP_CLI::line( 'Launching a spare site' );
 			}
+			$app = launch_wordpress( 'default', [], true );
+			\WP_CLI::line( sprintf( 'Launched spare site', $app->domains[0] ) );
 			return;
 		}
 		$app = launch_wordpress();
