eee1f8d1aec60e17cd6d8c2f9cdffb54a56be531
Brandon Kraft <public@brandonkraft.com>
2022-04-19 13:44:52 -0500

    Version bump

--- jurassic.ninja.php
+++ jurassic.ninja.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Jurassic Ninja
  * Description: Launch ephemeral instances of WordPress + Jetpack using ServerPilot and an Ubuntu Box.
- * Version: 5.15
+ * Version: 5.16
  * Author: Automattic
  *
  * @package jurassic-ninja
