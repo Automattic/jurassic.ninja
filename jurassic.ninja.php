e59e0819d8d4f64888194f18d1767a6876942164
Sam Seay <samueljseay@gmail.com>
2023-01-18 18:55:23 +1300

    Revert testing changes.

--- jurassic.ninja.php
+++ jurassic.ninja.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Jurassic Ninja
  * Description: Launch ephemeral instances of WordPress + Jetpack using ServerPilot and an Ubuntu Box.
- * Version: 5.20
+ * Version: 5.21
  * Author: Automattic
  *
  * @package jurassic-ninja
@@ -47,7 +47,7 @@ function init() {
 	// Create settings page.
 	add_settings_page();
 	// Settings problems include credentials and IDs not configured.
-	// if ( ! settings_problems() ) {
+	if ( ! settings_problems() ) {
 		// Include the JS only under the page which has the /create slug.
 		add_scripts();
 		// Serve the API root and nonce only under the page which has the /create slug.
@@ -57,7 +57,7 @@ function init() {
 		// Disable temporarily. Run via crontab and Jurassic Ninja's CLI
 		// add_cron_job( __FILE__ );.
 		add_admin_bar_node();
-	// }
+	}
 	/**
 	 * Done after adding settings page and before anything else related to Jurassic Ninja specifics.
 	 *
@@ -146,7 +146,7 @@ function add_scripts() {
 	add_action(
 		'wp_enqueue_scripts',
 		function () {
-			if ( true ) {
+			if ( page_is_launching_page() ) {
 				wp_enqueue_script( 'jurassicninja.js', plugins_url( '', __FILE__ ) . '/jurassicninja.js', array( 'jquery' ), '1.1', true );
 				/**
 				 * Done after enqueueing the jurassic.ninja.js file
